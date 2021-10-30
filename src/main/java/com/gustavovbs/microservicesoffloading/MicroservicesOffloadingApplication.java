8809cb1cff02d50b8df614caf9c70b0d48205b18
Gustavo Vilas Boas <gustavovbs98@gmail.com>
2021-10-30 16:23:21 -0300

    Project Creation

---
+++ src/main/java/com/gustavovbs/microservicesoffloading/MicroservicesOffloadingApplication.java
@@ -0,0 +1,13 @@
+package com.gustavovbs.microservicesoffloading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesOffloadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesOffloadingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gustavovbs/microservicesoffloading/MicroservicesOffloadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavovbs.microservicesoffloading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesOffloadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
