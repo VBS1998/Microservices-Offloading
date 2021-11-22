6309d17beb5a4a185ce16646b1faefcb1e5e0f96
Gustavo Vilas Boas <gustavovbs98@gmail.com>
2021-11-22 14:06:03 -0300

    Returns added url

--- src/main/java/com/gustavovbs/broker/BrokerController.java
+++ src/main/java/com/gustavovbs/broker/BrokerController.java
@@ -2,10 +2,7 @@ package com.gustavovbs.broker;
 
 import com.gustavovbs.microservicesoffloading.Auction;
 import com.gustavovbs.microservicesoffloading.MicroservicesOffloadingController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
@@ -29,13 +26,13 @@ public class BrokerController {
     }
 
     @PostMapping("/broadcast")
-    public String broadcast(Auction auction){
+    public String broadcast(@RequestBody Auction auction){
         return broker.broadcast(auction);
     }
 
     @PostMapping("/add_host")
     public String addHost(URI host) {
         broker.addHost(host);
-        return "ok";
+        return "ok " + host.toString();
     }
 }
