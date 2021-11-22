6fed9e2dbb5c41705180ec733300b990f2a916a9
Gustavo Vilas Boas <gustavovbs98@gmail.com>
2021-11-22 17:31:47 -0300

    adding auction and winner to return

--- src/main/java/com/gustavovbs/broker/Broker.java
+++ src/main/java/com/gustavovbs/broker/Broker.java
@@ -31,7 +31,7 @@ public class Broker {
 
         //After the auction has ended
         URI winner = auction.close().getHost();
-        return rest.postForEntity(winner + "/run", auction.getMicroserviceName(), String.class).getBody();
+        return rest.postForEntity(winner + "/run", auction.getMicroserviceName(), String.class).getBody() + auction.toString() + winner.toString();
     }
 
     public void addHost(URI host){
