571cdbe02752ca4571ec7b242fd26c9d4392ec04
Gustavo Vilas Boas <gustavovbs98@gmail.com>
2021-11-24 12:03:32 -0300

    Creating rooter service

---
+++ src/main/java/com/gustavovbs/rooter/Rooter.java
@@ -0,0 +1,11 @@
+package com.gustavovbs.rooter;
+import java.lang.Math;
+
+public class Rooter {
+
+    public Rooter(){}
+
+    public double root(double x){
+        return Math.sqrt(x);
+    }
+}
---
+++ src/main/java/com/gustavovbs/rooter/RooterController.java
@@ -0,0 +1,22 @@
+package com.gustavovbs.rooter;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/rooter")
+public class RooterController {
+
+    private Rooter rooter;
+
+    @GetMapping("/")
+    public String runRooter(){
+        rooter = new Rooter();
+        return "Rooter is running";
+    }
+
+    @PostMapping("/sqrt")
+    public Double sqrt(@RequestBody double x){
+        return rooter.root(x);
+    }
+
+}
