9dc6e20ac42142bcefcac496ac5b941666e9bc2a
Gustavo Vilas Boas <gustavovbs98@gmail.com>
2021-12-04 18:34:02 -0300

    Matrix Microservice

---
+++ src/main/java/com/gustavovbs/matrix/Matrix.java
@@ -0,0 +1,30 @@
+package com.gustavovbs.matrix;
+
+import java.util.Random;
+
+public class Matrix {
+
+    public Double[][] def(int seed, int size){
+        Random r = new Random((long) seed);
+
+        Double a[][] = new Double[size][size];
+        Double b[][] = new Double[size][size];
+        Double c[][] = new Double[size][size];
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                a[i][j] = r.nextDouble() * r.nextInt();
+                b[i][j] = r.nextDouble() * r.nextInt();
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for (int k = 0; k < size; k++)
+                    c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+
+        return c;
+    }
+}
---
+++ src/main/java/com/gustavovbs/matrix/MatrixController.java
@@ -0,0 +1,36 @@
+package com.gustavovbs.matrix;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Serializable;
+
+@RestController
+@RequestMapping("/matrix")
+public class MatrixController {
+
+    public class DefBody implements Serializable {
+        public int seed, size;
+        public DefBody(int seed, int size){
+            this.seed = seed;
+            this.size = size;
+        }
+        public int getSeed() { return seed; }
+        public void setSeed(int seed) { this.seed = seed; }
+        public int getSize() { return size; }
+        public void setSize(int size) { this.size = size; }
+    }
+
+    private Matrix matrix;
+
+    @GetMapping("/")
+    public String runRooter(){
+        matrix = new Matrix();
+        return "Rooter is running";
+    }
+
+    @GetMapping("/default")
+    public String def(@RequestBody DefBody body){
+        return matrix.def(body.seed, body.size).toString();
+    }
+
+}
